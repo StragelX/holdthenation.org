829c550b76f184c9f85e9498db7339259317d7fd
StragelX <stragelx@gmail.com>
2022-06-14 16:16:27 +0300

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+  function myFunction1() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput1");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
 });
